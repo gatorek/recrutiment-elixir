ff83ed8e320f4b82a0faa144211c83bc032c460d
Michał Sacewicz <michal@sacewicz,pl>
2020-02-18 20:23:41 +0100

    bugfix

--- lib/gamecode_web/services/week_stat_service.ex
+++ lib/gamecode_web/services/week_stat_service.ex
@@ -2,7 +2,7 @@ defmodule GamecodeWeb.WeekStatService do
 
   def get(data) do
     data |> Enum.reduce(
-        %{total_distance: 0, total_price: 0},
+        %{total_distance: 0.0, total_price: 0.0},
         &(%{
           total_distance: &2.total_distance + &1.distance,
           total_price: &2.total_price + &1.price,
@@ -12,6 +12,5 @@ defmodule GamecodeWeb.WeekStatService do
         total_distance: Float.round(&1.total_distance, 1),
         total_price: Float.round(&1.total_price, 2)
       })).()
-
   end
 end
